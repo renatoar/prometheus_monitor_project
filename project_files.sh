0c8fa646860b40e0767478599d6e1ae80bf3812f
renatoar <renato.rizzo@dcomp.sor.ufscar.br>
2019-07-01 18:02:00 -0300

    Visual code Project

---
+++ project_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/renatoar/prometheus_monitoring_docker.git
+git clone https://github.com/renatoar/monitoring_container.git
+git clone https://github.com/renatoar/vm_monitoring_container.git
+git clone https://github.com/renatoar/transferfileapp_container.git
